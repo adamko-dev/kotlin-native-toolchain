7aadc10b26f689f1ef7e2ce8e56274c7bbf7da9c
Adam <897017+aSemy@users.noreply.github.com>
2026-01-31 13:23:34 +0100

    rm old files

--- knt-tools/knp-dependencies-data-model/src/main/kotlin/HostOs.kt
+++
@@ -1,9 +0,0 @@
-package dev.adamko.kntoolchain.tools.datamodel
-
-//import kotlinx.serialization.Serializable
-//
-//@Serializable
-//data class HostOs(
-//  val family: String,
-//  val arch: String,
-//)
